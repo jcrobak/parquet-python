d9429d5e1584f71e4b86d638a93c550906636fc9
Joe Crobak <joecrow@gmail.com>
2016-08-16 01:31:54 +0000

    bump version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ except ImportError:
     from distutils.core import setup
 
 setup(name='parquet',
-    version='1.0',
+    version='1.1',
     description='Python support for Parquet file format',
     author='Joe Crobak',
     author_email='joecrow@gmail.com',
