cf48f9c4a8e10c353e4167dbb30ed6dd4a2f9a13
Martin Durant <martin.durant@utoronto.ca>
2016-10-18 14:06:05 -0400

    Tidy category/partition stuff

--- parquet/__init__.py
+++ parquet/__init__.py
@@ -30,6 +30,7 @@ class ParquetFile(object):
             f = fname
         self.fname = fname
         self._parse_header(f, verify)
+        self._read_partitions()
 
     def _parse_header(self, f, verify=True):
         try:
@@ -61,12 +62,21 @@ class ParquetFile(object):
     def columns(self):
         return [f.name for f in self.schema if f.num_children is None]
 
+    def _read_partitions(self):
+        cats = {}
+        for rg in self.row_groups:
+            for col in rg.columns:
+                partitions = re.findall("([a-zA-Z_]+)=([^/]+)/",
+                                        col.file_path or "")
+                for key, val in partitions:
+                    cats.setdefault(key, set()).add(val)
+        self.cats = {key: list(v) for key, v in cats.items()}
+
     def to_pandas(self, columns=None):
         cols = columns or self.columns
         import pandas as pd
         tot = []
-        cats = {}
-        for i, rg in enumerate(self.row_groups):
+        for rg in self.row_groups:
             # read values
             out = pd.DataFrame()
             for col in rg.columns:
@@ -77,26 +87,21 @@ class ParquetFile(object):
                                     self.fname)
                 out[name] = s
 
-                partitions = re.findall("([a-zA-Z_]+)=([^/]+)/",
-                                        col.file_path or "")
-                for key, val in partitions:
-                    cats.setdefault(key, set()).add(val)
-
-            tot.append(out)
-
-        cats = {key: list(v) for key, v in cats.items()}
-        for out, rg in zip(tot, self.row_groups):
-            for cat in cats:
+            # apply categories
+            for cat in self.cats:
                 # *Hard assumption*: all chunks in a row group have the
                 # same partition (correct for spark/hive)
                 partitions = re.findall("([a-zA-Z_]+)=([^/]+)/",
                                         rg.columns[0].file_path)
                 val = [p[1] for p in partitions if p[0] == cat][0]
                 codes = np.empty(rg.num_rows, dtype=np.int16)
-                codes[:] = cats[cat].index(val)
-                out[cat] = pd.Categorical.from_codes(codes,
-                            [val_to_num(c) for c in cats[cat]])
+                codes[:] = self.cats[cat].index(val)
+                out[cat] = pd.Categorical.from_codes(
+                        codes, [val_to_num(c) for c in self.cats[cat]])
+            tot.append(out)
 
+        # TODO: if categories vary from one rg to next, need
+        # pandas.types.concat.union_categoricals
         return pd.concat(tot, ignore_index=True)
 
     def __str__(self):
