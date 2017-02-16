12a47364e5f22355e19725157adc1f114cc485b3
Martin Durant <martin.durant@utoronto.ca>
2017-02-16 15:08:31 -0500

    Mark test to skip
    pip's bson (as opposed to ones from conda) implicitly expect BSON
    string values to be ASCII.

--- fastparquet/test/test_converted_types.py
+++ fastparquet/test/test_converted_types.py
@@ -86,6 +86,7 @@ def test_json():
                           schema)[0] == {'foo': ['bar', u'👾']}
 
 
+@pytest.mark.skipif(PY2,reason='BSON unicode may not be supported in Python 2')
 def test_bson():
     """Test bytes representing bson."""
     bson = pytest.importorskip('bson')
