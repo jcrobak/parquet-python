9bd5beb885caceacbfec0138b3e09a3af38d7705
Martin Durant <martin.durant@utoronto.ca>
2017-06-13 12:24:37 -0400

    Bump version 0.1.0

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ ext_modules = cythonize(cython_modules)
 
 setup(
     name='fastparquet',
-    version='0.0.6',
+    version='0.1.0',
     description='Python support for Parquet file format',
     ext_modules=ext_modules,
     author='Martin Durant',
