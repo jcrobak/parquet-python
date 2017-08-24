9925b27a1bc13cbf153b1261daa4967c70d002ed
Martin Durant <martin.durant@utoronto.ca>
2017-08-24 14:16:25 -0400

    Revert accidental deletion

--- fastparquet/test/test_api.py
+++ fastparquet/test/test_api.py
@@ -6,6 +6,7 @@ import numpy as np
 import pandas as pd
 import pytest
 
+from fastparquet.util import tempdir
 from fastparquet import write, ParquetFile
 from fastparquet.api import statistics, sorted_partitioned_columns
 
