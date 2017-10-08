9ed5544d23c375f96273dd141adb521c49390a2c
Martin Durant <martin.durant@utoronto.ca>
2017-10-08 10:04:17 -0400

    v0.1.3

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ else:
 
 setup(
     name='fastparquet',
-    version='0.1.2',
+    version='0.1.3',
     description='Python support for Parquet file format',
     author='Martin Durant',
     author_email='mdurant@continuum.io',
