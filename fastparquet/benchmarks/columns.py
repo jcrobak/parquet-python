90e31d6ec41b0c5eae28f5bb8e0277add2be8587
Kyle Lahnakoski <klahnakoski@mozilla.com>
2018-01-17 16:59:04 -0500

    fixes to get benchmark to work (#276)
    * fixes to get benchmark to work
    
    * use dask tmpdir

--- fastparquet/benchmarks/columns.py
+++ fastparquet/benchmarks/columns.py
@@ -1,13 +1,14 @@
-from contextlib import contextmanager
-import os
 import numpy as np
+import os
 import pandas as pd
+import shutil
+import sys
+import tempfile
 import time
-
-from fastparquet.util import join_path
+from contextlib import contextmanager
 
 from fastparquet import write, ParquetFile
-from dask.utils import tmpdir
+from fastparquet.util import join_path
 
 
 @contextmanager
@@ -23,7 +24,7 @@ def time_column():
         result = {}
         fn = join_path(tempdir, 'temp.parq')
         n = 10000000
-        r = np.random.randint(-1e10, 1e10, n)
+        r = np.random.randint(-1e10, 1e10, n, dtype='int64')
         d = pd.DataFrame({'w': pd.Categorical(np.random.choice(
                 ['hi', 'you', 'people'], size=n)),
                           'x': r.view('timedelta64[ns]'),
@@ -110,14 +111,6 @@ def time_text():
         return result
 
 
-if __name__ == '__main__':
-    result = {}
-    for f in [time_column, time_text]:
-        result.update(f())
-    for k in sorted(result):
-        print(k, result[k])
-
-
 def time_find_nulls(N=10000000):
     x = np.random.random(N)
     df = pd.DataFrame({'x': x})
@@ -164,4 +157,43 @@ def run_find_nulls(df, res):
         df.x.notnull().all()
     with measure((df.x.dtype.kind, nvalid, 'count'), res):
         df.x.count()
+
+
+
+# from https://github.com/dask/dask/blob/6cbcf0813af48597a427a1fe6c71cce2a79086b0/dask/utils.py#L78
+@contextmanager
+def ignoring(*exceptions):
+    try:
+        yield
+    except exceptions:
+        pass
+
+# from https://github.com/dask/dask/blob/6cbcf0813af48597a427a1fe6c71cce2a79086b0/dask/utils.py#L116
+@contextmanager
+def tmpdir(dir=None):
+    dirname = tempfile.mkdtemp(dir=dir)
+
+    try:
+        yield dirname
+    finally:
+        if os.path.exists(dirname):
+            if os.path.isdir(dirname):
+                with ignoring(OSError):
+                    shutil.rmtree(dirname)
+            else:
+                with ignoring(OSError):
+                    os.remove(dirname)
+
+
+if __name__ == '__main__':
+    result = {}
+
+    print("sys.version = " + sys.version)
+    print("sys.platform = " + sys.platform)
+
+    for f in [time_column, time_text]:
+        result.update(f())
+    for k in sorted(result):
+        print(k, result[k])
+
 
